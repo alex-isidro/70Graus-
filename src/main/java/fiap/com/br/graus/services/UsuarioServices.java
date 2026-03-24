bb2700f57661485a38eaf52bc25ad8cbaaeffdd4
alex-isidro <alexander.d.isidro@gmail.com>
2026-03-24 11:49:13 -0300

    feat: Implementa service para gerenciamento de usuários

--- src/main/java/fiap/com/br/graus/services/UsuarioServices.java
+++ src/main/java/fiap/com/br/graus/services/UsuarioServices.java
@@ -1,4 +1,43 @@
 package fiap.com.br.graus.services;
 
+import fiap.com.br.graus.model.Usuario;
+import fiap.com.br.graus.repositories.UsuarioRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UsuarioServices {
+    @Autowired
+    private UsuarioRepositories repositories;
+
+    private Usuario findUsuarioById(Long id) {
+        return repositories.findById(id).orElseThrow(
+                () -> new RuntimeException("Despesas com id " + id + " não encontrado")
+        );
+    }
+
+    public List<Usuario> getAllUsuario(){
+        return repositories.findAll();
+    }
+
+    public Usuario addUsuario(Usuario usuario){
+        return repositories.save(usuario);
+    }
+
+    public Usuario getUsuarioById(Long id){
+        return findUsuarioById(id);
+    }
+
+    public void deleteUsuario(Long id) {
+        findUsuarioById(id);
+        repositories.deleteById(id);
+    }
+
+    public Usuario updateUsuario(Long id, Usuario newUsuario) {
+        findUsuarioById(id);
+        newUsuario.setId(id);
+        return repositories.save(newUsuario);
+    }
 }
