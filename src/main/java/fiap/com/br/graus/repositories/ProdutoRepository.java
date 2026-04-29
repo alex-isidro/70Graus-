49552f98d319c3361274d58033fef344a6248584
alex-isidro <alexander.d.isidro@gmail.com>
2026-04-28 23:50:09 -0300

    feat: adiciona métodos de busca por nome, preço e tamanho no ProdutoController e ProdutoService

--- src/main/java/fiap/com/br/graus/repositories/ProdutoRepository.java
+++ src/main/java/fiap/com/br/graus/repositories/ProdutoRepository.java
@@ -3,5 +3,14 @@ package fiap.com.br.graus.repositories;
 import fiap.com.br.graus.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    List<Produto> findByPrecoBetween(BigDecimal min, BigDecimal max);
+
+    List<Produto> findByNomeContainingIgnoreCase(String nome);
+
+    List<Produto> findByTamanhoIgnoreCase(String tamanho);
 }
